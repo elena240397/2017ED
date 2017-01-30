ee7bd2e39322247c0f67529184ceacc7ebdd6c2f
pepejuan <pepe.juan@gmail.com>
2017-01-30 17:05:57 +0100

    Segunda versión

--- src/pruebaGitHub/Prueba.java
+++ src/pruebaGitHub/Prueba.java
@@ -3,11 +3,11 @@ package pruebaGitHub;
 public class Prueba {
 
 	public static void main(String[] args) {
-		int a = 2;
+		int a = 9;
 		int b = 32;
 		int c = 4500;
 		for (int count = 1; count <= 6; count = count + 1) {
-			System.out.println(count + ".  Sleep");
+			System.out.println(count + ".  EAt");
 		}
 		System.out.println(a + b + c);
 
