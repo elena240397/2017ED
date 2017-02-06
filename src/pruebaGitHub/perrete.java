7baa03720e6af5f8ed60702410a396b8e3482583
Elena Roman <elenarompiz@gmail.com>
2017-02-06 16:28:20 +0100

    añadir perrete Rama1

---
+++ src/pruebaGitHub/perrete.java
@@ -0,0 +1,23 @@
+package pruebaGitHub;
+
+public class perrete {
+
+		int numero_inicio = 19;
+
+		/**
+		 * @return the numero_inicio
+		 */
+		public int getNumero_inicio() {
+			return numero_inicio;
+		}
+
+		/**
+		 * @param numero_inicio the numero_inicio to set
+		 */
+		public void setNumero_inicio(int numero_inicio) {
+			this.numero_inicio = numero_inicio;
+		}
+		
+	
+
+}
