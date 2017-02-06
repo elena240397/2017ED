3d4de7d463183c18bb1c13d194188aed49000822
Elena Roman <elenarompiz@gmail.com>
2017-02-06 16:52:15 +0100

    cambio

--- src/pruebaGitHub/gatete.java
+++ src/pruebaGitHub/gatete.java
@@ -2,4 +2,19 @@ package pruebaGitHub;
 
 public class gatete {
 	int numero_vidas;
+
+	/**
+	 * @return the numero_vidas
+	 */
+	public int getNumero_vidas() {
+		return numero_vidas;
+	}
+
+	/**
+	 * @param numero_vidas the numero_vidas to set
+	 */
+	public void setNumero_vidas(int numero_vidas) {
+		this.numero_vidas = numero_vidas;
+	}
+	
 }
